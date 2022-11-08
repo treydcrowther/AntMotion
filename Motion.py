ffce7d69c9c9e1b103c4b7b73c1d6cb0eeff5ae1
Trey Crowther <trey.crowther@aggiemail.usu.edu>
2022-11-08 01:14:45 -0700

    Initial code with servo number definition

---
+++ Motion.py
@@ -0,0 +1,73 @@
+import serial
+
+######################################################
+# Leg Servo Key
+# First letter: Side (R = Right, L = Left)
+# Second Letter: Leg (R = Rear, M = Middle, F = Front)
+# Third Letter: Servo (H = Hip, M = Middle, K = Knee)
+######################################################
+
+## Right Leg Definitions
+RRH = 0
+RRM = 1
+RRK = 2
+RMH = 4
+RMM = 5
+RMK = 6
+RFH = 8
+RFM = 9
+RFK = 10
+## Left Leg Definitions
+LRH = 16
+LRM = 17
+LRK = 18
+LMH = 20
+LMM = 21
+LMK = 22
+LFH = 24
+LFM = 25
+LFK = 26
+
+#######################
+# Mandibles Servo Key
+# First letter: M = Mandibles
+# Second letter: Servo (R = Rotation, L = Left Grip, R = Right Grip, T = Tilt)
+####################### 
+MR = 11
+ML = 12
+MR = 13
+MT = 14
+
+#######################
+# Tail Servo Key
+# First letter: T = Tail
+# Second letter: Servo (T = Tilt, R = Rotation)
+####################### 
+TT = 27
+TR = 28
+
+# Serial Command Port
+scp = '/dev/ttyS0'
+baudRate = 9600
+
+
+def SetPos(motor, position):
+    return f"#{motor} P{position}"
+
+def AllCentered(ser):
+    command = ""
+    for i in range(32):
+        command += SetPos(i, 1500) + " "
+    ser.write(str.encode(command + "\r"))
+
+def Stand(ser):
+    # Move all legs towards body and then stand
+    x = 0
+
+
+def main():
+    ser = serial.Serial(scp, baudRate)
+    AllCentered(ser)
+
+
+main()
