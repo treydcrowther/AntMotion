b450954c71161b97269e8512a38a692160a65a06
Trey Crowther <trey.crowther@aggiemail.usu.edu>
2022-12-16 17:51:36 -0700

    Fixed Motion.py

--- Motion.py
+++ Motion.py
@@ -1,345 +1,213 @@
 import serial
 import time
 
-######################################################
-# Leg Servo Key
-# First letter: Side (R = Right, L = Left)
-# Second Letter: Leg (R = Rear, M = Middle, F = Front)
-# Third Letter: Servo (H = Hip, M = Middle, K = Knee)
-######################################################
+class Motion:
+    ser = None
+    baudRate = None
+    scp = None
+    def __init__(self):
+        self.scp = '/dev/ttyS0'
+        self.baudRate = 9600
+        self.ser = serial.Serial(self.scp, self.baudRate, timeout=1)
 
-## Right Leg Definitions
-LRH = 0
-LRK = 1
-LRA = 2
-LMH = 4
-LMK = 5
-LMA = 6
-LFH = 8
-LFK = 9
-LFA = 10
+    ######################################################
+    # Leg Servo Key
+    # First letter: Side (R = Right, L = Left)
+    # Second Letter: Leg (R = Rear, M = Middle, F = Front)
+    # Third Letter: Servo (H = Hip, M = Middle, K = Knee)
+    ######################################################
+    ## Right Leg Definitions
+    __LRH = 0
+    __LRK = 1
+    __LRA = 2
+    __LMH = 4
+    __LMK = 5
+    __LMA = 6
+    __LFH = 8
+    __LFK = 9
+    __LFA = 10
+    ## Left Leg Definitions
+    __RRH = 16
+    __RRK = 17
+    __RRA = 18
+    __RMH = 20
+    __RMK = 21
+    __RMA = 22
+    __RFH = 24
+    __RFK = 25
+    __RFA = 26
 
+    ###############################################################################
+    # Mandibles Servo Key
+    # First letter: M = Mandibles
+    # Second letter: Servo (R = Rotation, L = Left Grip, R = Right Grip, T = Tilt)
+    ###############################################################################
+    __MY = 12
+    __MR = 13
+    __MT = 14
+    __MLG = 28
+    __MRG = 29
+    __MandibleServos = [__MY,__MR,__MT,__MLG,__MRG]
 
-## Left Leg Definitions
-RRH = 16
-RRK = 17
-RRA = 18
-RMH = 20
-RMK = 21
-RMA = 22
-RFH = 24
-RFK = 25
-RFA = 26
+    ################################################
+    # Tail Servo Key
+    # First letter: T = Tail
+    # Second letter: Servo (T = Tilt, R = Rotation)
+    ################################################
+    __TR = 30
+    __TT = 31
+    __TailServos = [__TR,__TT]
 
-leftAnkles = [LRA, LMA, LFA]
-rightAnkles = [RRA, RMA, RFA]
+    # Stand Position
+    __standPosition = {__LFH:1400, __LFK:1400, __LFA:1400, __LMH:1500, __LMK:1400, __LMA:1400, __LRH:1400, __LRK:1600, __LRA:1400, __RRH:1500, __RRK:1600, __RRA:1600, __RMH:1500, __RMK:1650, __RMA:1600, __RFH:1700, __RFK:1600, __RFA:1600  }
+    __sitPosition = {__LFH:1500, __LFK:750, __LFA:750, __LMH:1500, __LMK:750, __LMA:750, __LRH:1500, __LRK:850, __LRA:750, __RRH:1500, __RRK:2300, __RRA:2300, __RMH:1500, __RMK:2300, __RMA:2300, __RFH:1500, __RFK:2300, __RFA:2300  }
 
-leftHips = [LRH, LMH, LFH]
-rightHips = [RFH, RMH, RRH]
+    # Movement Positions
+    __walkPositionOne = {__LFH:1100, __LFK:1000, __LFA:1400, __LMH:1900, __LMK:1700, __LMA:1400, __LRH:1200, __LRK:1000, __LRA:1200, __RRH:1100, __RRK:1600, __RRA:1700, __RMH:1500, __RMK:2000, __RMA:1600, __RFH:1500, __RFK:1600, __RFA:1600  }
+    __walkPositionTwo = {__LFH:1100, __LFK:1800, __LFA:1400, __LMH:1900, __LMK:1700, __LMA:1400, __LRH:1200, __LRK:1800, __LRA:1200, __RRH:1100, __RRK:1600, __RRA:1700, __RMH:1500, __RMK:1200, __RMA:1600, __RFH:1500, __RFK:1600, __RFA:1600  }
+    __walkPositionThree = {__LFH:1500, __LFK:1800, __LFA:1400, __LMH:1500, __LMK:1000, __LMA:1400, __LRH:1600, __LRK:1800, __LRA:1200, __RRH:1500, __RRK:2000, __RRA:1700, __RMH:1100, __RMK:1200, __RMA:1600, __RFH:1600, __RFK:2000, __RFA:1625  }
+    __walkPositionFour = {__LFH:1500, __LFK:1800, __LFA:1400, __LMH:1500, __LMK:1800, __LMA:1400, __LRH:1600, __LRK:1800, __LRA:1200, __RRH:1500, __RRK:1200, __RRA:1700, __RMH:1100, __RMK:1200, __RMA:1600, __RFH:1600, __RFK:1200, __RFA:1625 }
 
-leftKnees = [LRK, LMK, LFK]
-rightKnees = [RFK, RMK, RRK]
+    # Rotation Positions
+    __rotateRightPosition = { __LFH:1800, __LRH:1800, __LMH:1800, __RMH:1800, __RRH:1800, __RFH:1800}
+    __rotateLeftPosition = { __LFH:1100, __LRH:1100, __LMH:1100, __RMH:1100, __RRH:1100, __RFH:1100}
+    __minimalRotateRightPosition = { __LFH:1700, __LRH:1700, __LMH:1700, __RMH:1700, __RRH:1700, __RFH:1700}
+    __minimalRotateLeftPosition = { __LFH: 1400, __LRH: 1400, __LMH: 1400, __RMH: 1400, __RRH: 1400, __RFH: 1400}
 
-frontHips = [[RFH], [LFH]]
-middleHips = [[RMH], [LMH]]
-backHips = [[RRH], [LRH]]
-
-# [0] = hips [1] = knees [2] = ankles
-# [][0] = leftside [][1] = rightside
-leftWalkMotors = [[[LFH, LRH], [RMH]], [[LFK, LRK] , [RMK]], [[LFA, LRA],  [RMA]]]
-rightWalkMotors = [[[LMH], [RFH, RRH]], [[LMK], [RFK, RRK]], [[LMA], [RFA, RRA]]]
-
-
-###############################################################################
-# Mandibles Servo Key
-# First letter: M = Mandibles
-# Second letter: Servo (R = Rotation, L = Left Grip, R = Right Grip, T = Tilt)
-###############################################################################
-MY = 12
-MR = 13
-MT = 14
-MLG = 28
-MRG = 29
-
-MandibleServos = [MY,MR,MT,MLG,MRG]
-
-################################################
-# Tail Servo Key
-# First letter: T = Tail
-# Second letter: Servo (T = Tilt, R = Rotation)
-################################################
-TR = 30
-TT = 31
-
-TailServos = [TR,TT]
-
-# Serial Command Port
-scp = '/dev/ttyS0'
-baudRate = 9600
-
-
-
-def SetPos(motor, position):
-    return f"#{motor} P{position} "
-
-def MoveSingleMotor(ser, motor, position):
-    ser.write(str.encode(SetPos(motor, position) + "T200\r"))
-
-def AllCentered(ser):
-    command = ""
-    for i in range(32):
-        command += SetPos(i, 1500)
-    ser.write(str.encode(command + "\r"))
-
-def CenterHeadAndTail(ser):
-    command = ""
-
-    for servo in MandibleServos:
-        command += SetPos(servo, 1500) + " "
-    ser.write(str.encode(command + "\r"))
-
-    for servo in TailServos:
-        command += SetPos(servo, 1500) + " "
-    command += SetPos(MT, 1500) + " "
-    command += SetPos(MRG, 1700) + " "
-    command += SetPos(MLG, 900) + " "
-    command += SetPos(MY, 1500) + " "
-    ser.write(str.encode(command + "\r"))
-
-def ComputeRightPosition(position):
-    if(position > 1500):
-        adjustedPosition = 1500 - (position - 1500)
-    else:
-        adjustedPosition = 1500 + (1500 - position)
-    return adjustedPosition
-
-def SetAnklePosition(ser):
-    command = ""
-    for ankle in leftAnkles:
-        command += SetPos(ankle,700)
-    for ankle in rightAnkles:
-        command += SetPos(ankle,2300)    
-    ser.write(str.encode(command + "T200\r"))
-
-
-def ControlBothSides(ser, leftArray, rightArray, position):
-    CommandArrayToPosition(ser, leftArray, position)
-    CommandArrayToPosition(ser, rightArray, ComputeRightPosition(position))
-
-def CommandArrayToPosition(ser, array, position):
-    command = ""
-    for servo in array:
-        command += SetPos(servo, position)
-    ser.write(str.encode(command + "T1000\r"))   
-
-def CommandDictToPosition(ser, dict, time= 200):
-    command = ""
-    for key in dict:
-        command += SetPos(key, dict[key])
-    ser.write(str.encode(command + f"T{time}\r"))
-
-def Sit(ser):
-    SittingPosition(ser)
-    time.sleep(2)
-
-
-standPosition = {LFH:1400, LFK:1400, LFA:1400, LMH:1500, LMK:1400, LMA:1400, LRH:1400, LRK:1600, LRA:1400, RRH:1500, RRK:1600, RRA:1600, RMH:1500, RMK:1650, RMA:1600, RFH:1700, RFK:1600, RFA:1600  }
-sitPosition = {LFH:1500, LFK:750, LFA:750, LMH:1500, LMK:750, LMA:750, LRH:1500, LRK:850, LRA:750, RRH:1500, RRK:2300, RRA:2300, RMH:1500, RMK:2300, RMA:2300, RFH:1500, RFK:2300, RFA:2300  }
-openMandibles = {MRG: 1300, MLG: 1300}
-reachDownPosition = {MT:1900}
-grabPosition = {MLG: 600, MRG: 1700}
-liftPosition =  {MT:1000}
-
-def PickupManeuver(ser, steps = 3):
-    CommandDictToPosition(ser, openMandibles, 200)
-    for i in range(0, steps):
-        WalkOneStep(ser)
-    ReachDownAndGrab(ser)
+    # Move Back to Center after Rotation
+    __moveBackToCenterOne = { __LFK: 1000, __LFH: 1400, __RRK: 2000, __RRH: 1500}
+    __moveBackToCenterTwo = { __LFK: 1400, __RRK: 1600, __LFA: 1400, __RRA: 1600}
+    __moveBackToCenterThree = { __LMK: 1000, __LMH: 1500, __RMK: 2000, __RMH: 1500}
+    __moveBackToCenterFour = { __LMK: 1400, __RMK: 1600, __LMA: 1400, __RMA: 1600}
+    __moveBackToCenterFive = { __LRK: 1000, __LRH: 1400, __RFK: 2000, __RFH: 1700}
+    __moveBackToCenterSix = { __LRK: 1400, __RFK: 1600, __LRA: 1400, __RRA: 1600}
     
-def ReachDownAndGrab(ser):
-    CommandDictToPosition(ser, openMandibles, 200)
-    time.sleep(1)
-    CommandDictToPosition(ser, reachDownPosition, 200)
-    time.sleep(1)
-    CommandDictToPosition(ser, grabPosition, 200)
-    time.sleep(1)
-    CommandDictToPosition(ser, liftPosition, 200)
+    # Head Positions
+    __openMandibles = {__MRG: 1300, __MLG: 1300}
+    __reachDownPosition = {__MT:1900}
+    __grabPosition = {__MLG: 600, __MRG: 1700}
+    __liftPosition =  {__MT:1000}
 
 
-def Stand(ser):
-    # Move all legs towards body and then stand
-    Sit(ser)
-    StandingPosition(ser)
-    # time.sleep(2)
-    # ControlBothSides(ser, leftAnkles, rightAnkles, 1600)    
-    # ControlBothSides(ser, leftKnees, rightKnees, 1600) 
-    time.sleep(2)
+    def __SetPos(self, motor, position):
+        return f"#{motor} P{position} "
 
-
-# Move the right leg farther forward on the step to counteract right movement
-walkPositionOne = {LFH:1100, LFK:1000, LFA:1400, LMH:1900, LMK:1700, LMA:1400, LRH:1200, LRK:1000, LRA:1200, RRH:1100, RRK:1600, RRA:1700, RMH:1500, RMK:2000, RMA:1600, RFH:1500, RFK:1600, RFA:1600  }
-walkPositionTwo = {LFH:1100, LFK:1800, LFA:1400, LMH:1900, LMK:1700, LMA:1400, LRH:1200, LRK:1800, LRA:1200, RRH:1100, RRK:1600, RRA:1700, RMH:1500, RMK:1200, RMA:1600, RFH:1500, RFK:1600, RFA:1600  }
-walkPositionThree = {LFH:1500, LFK:1800, LFA:1400, LMH:1500, LMK:1000, LMA:1400, LRH:1600, LRK:1800, LRA:1200, RRH:1500, RRK:2000, RRA:1700, RMH:1100, RMK:1200, RMA:1600, RFH:1600, RFK:2000, RFA:1625  }
-walkPositionFour = {LFH:1500, LFK:1800, LFA:1400, LMH:1500, LMK:1800, LMA:1400, LRH:1600, LRK:1800, LRA:1200, RRH:1500, RRK:1200, RRA:1700, RMH:1100, RMK:1200, RMA:1600, RFH:1600, RFK:1200, RFA:1625 }
-
-rotateRightPosition = { LFH:1800, LRH:1800, LMH:1800, RMH:1800, RRH:1800, RFH:1800}
-rotateLeftPosition = { LFH:1100, LRH:1100, LMH:1100, RMH:1100, RRH:1100, RFH:1100}
-minimalRotateRightPosition = { LFH:1600, LRH:1600, LMH:1600, RMH:1600, RRH:1600, RFH:1600}
-minimalRotateLeftPosition = { LFH: 1400, LRH: 1400, LMH: 1400, RMH: 1400, RRH: 1400, RFH: 1400}
-
-standPosition = {LFH:1400, LFK:1400, LFA:1400, LMH:1500, LMK:1400, LMA:1400, LRH:1400, LRK:1600, LRA:1400, RRH:1500, RRK:1600, RRA:1600, RMH:1500, RMK:1650, RMA:1600, RFH:1700, RFK:1600, RFA:1600  }
-
-
-moveBackToCenterOne = { LFK: 1000, LFH: 1400, RRK: 2000, RRH: 1500}
-moveBackToCenterTwo = { LFK: 1400, RRK: 1600, LFA: 1400, RRA: 1600}
-moveBackToCenterThree = { LMK: 1000, LMH: 1500, RMK: 2000, RMH: 1500}
-moveBackToCenterFour = { LMK: 1400, RMK: 1600, LMA: 1400, RMA: 1600}
-moveBackToCenterFive = { LRK: 1000, LRH: 1400, RFK: 2000, RFH: 1700}
-moveBackToCenterSix = { LRK: 1400, RFK: 1600, LRA: 1400, RRA: 1600}
-
-
-def LowerHead(ser, extent = 1):
-    extent *= 150
-    CommandDictToPosition(ser, {MT: 1550 + extent}, 200)
-
-
-def CommandThreeLeg(ser, left):
-    if(left):
-        moveLegs = leftWalkMotors
-        liftLegs = rightWalkMotors
-    else:
-        liftLegs = leftWalkMotors
-        moveLegs  = rightWalkMotors
+    def SetSinglePos(self, motor, position):
+        self.ser.write(str.encode(self.__SetPos(motor, position) + "\r"))
     
-    # Lift knees and rotate hip
-    ControlBothSides(ser, moveLegs[0][0], moveLegs[0][1], 1300)
-    ControlBothSides(ser, moveLegs[1][0], moveLegs[1][1], 1000)
-    ControlBothSides(ser, liftLegs[0][0], liftLegs[0][1], 1700)
-    time.sleep(1)
-    # Replace leg on ground
-    ControlBothSides(ser, moveLegs[1][0], moveLegs[1][1], 1800)
-    # time.sleep(1)
-    # ControlBothSides(ser, moveLegs[0][0], moveLegs[0][1], 1700)
-    # ControlBothSides(ser, liftLegs[1][0], liftLegs[1][1], 1300)
+    def CenterHeadAndTail(self):
+        command = ""
+        for servo in self.__MandibleServos:
+            command += self.__SetPos(servo, 1500) + " "
+        self.ser.write(str.encode(command + "\r"))
 
-def StandingPosition(ser):
-    CommandDictToPosition(ser, standPosition, 200)
+        for servo in self.__TailServos:
+            command += self.__SetPos(servo, 1500) + " "
+        command += self.__SetPos(self.__MT, 1500) + " "
+        command += self.__SetPos(self.__MRG, 1700) + " "
+        command += self.__SetPos(self.__MLG, 900) + " "
+        command += self.__SetPos(self.__MY, 1500) + " "
+        self.ser.write(str.encode(command + "\r"))
 
-def SittingPosition(ser):
-    time.sleep(2)
-    CommandDictToPosition(ser, sitPosition, 200)
+    def __CommandDictToPosition(self, dict, time= 200):
+        command = ""
+        for key in dict:
+            command += self.__SetPos(key, dict[key])
+        self.ser.write(str.encode(command + f"T{time}\r"))
+
+    def __StandingPosition(self):
+        self.__CommandDictToPosition(self.__standPosition, 200)
+
+    def __MoveBackAfterRotation(self):
+        self.__CommandDictToPosition(self.__moveBackToCenterOne, 50)
+        time.sleep(.4)
+        self.__CommandDictToPosition(self.__moveBackToCenterTwo, 50)
+        time.sleep(.2)
+        self.__CommandDictToPosition(self.__moveBackToCenterThree, 50)
+        time.sleep(.2)
+        self.__CommandDictToPosition(self.__moveBackToCenterFour, 50)
+        time.sleep(.2)
+        self.__CommandDictToPosition(self.__moveBackToCenterFive, 50)
+        time.sleep(.2)
+        self.__CommandDictToPosition(self.__moveBackToCenterSix, 50)
 
 
-def WalkOneStep(ser):
-    CommandDictToPosition(ser, walkPositionOne, 50)
-    CommandDictToPosition(ser, walkPositionTwo, 50)
-    # StandingPosition(ser)
-    CommandDictToPosition(ser, walkPositionThree, 50)
-    CommandDictToPosition(ser, walkPositionFour, 50)
-    StandingPosition(ser)
+    def Sit(self):
+        """_summary_: Moves the robot to a sitting position."""
+        time.sleep(2)
+        self.__CommandDictToPosition(self.__sitPosition, 200)
+        time.sleep(2)
 
-# Rotate the robot 19 degrees
-def RotateRightOne(ser):
-    CommandDictToPosition(ser, rotateRightPosition, 50)
-    time.sleep(.2)
-    MoveBackAfterRotation(ser)
-    time.sleep(.2)
-    CommandDictToPosition(ser, standPosition, 50)
+    def PickupManeuver(self, steps = 3):
+        self.__CommandDictToPosition(self.__openMandibles, 200)
+        for i in range(0, steps):
+            self.WalkOneStep()
+        self.ReachDownAndGrab()
+        
+    def ReachDownAndGrab(self):
+        self.__CommandDictToPosition(self.__openMandibles, 200)
+        time.sleep(1)
+        self.__CommandDictToPosition(self.__reachDownPosition, 200)
+        time.sleep(1)
+        self.__CommandDictToPosition(self.__grabPosition, 200)
+        time.sleep(1)
+        self.__CommandDictToPosition(self.__liftPosition, 200)
 
-def RotateLeftOne(ser):
-    CommandDictToPosition(ser, rotateLeftPosition, 50)
-    time.sleep(.1)
-    MoveBackAfterRotation(ser)
-    time.sleep(.1)
-    CommandDictToPosition(ser, standPosition, 50)
 
-def MoveBackAfterRotation(ser):
-    CommandDictToPosition(ser, moveBackToCenterOne, 50)
-    time.sleep(.1)
-    CommandDictToPosition(ser, moveBackToCenterTwo, 50)
-    time.sleep(.1)
-    CommandDictToPosition(ser, moveBackToCenterThree, 50)
-    time.sleep(.1)
-    CommandDictToPosition(ser, moveBackToCenterFour, 50)
-    time.sleep(.1)
-    CommandDictToPosition(ser, moveBackToCenterFive, 50)
-    time.sleep(.1)
-    CommandDictToPosition(ser, moveBackToCenterSix, 50)
-    
-def MinimalRotateLeftOne(ser):
-    CommandDictToPosition(ser, minimalRotateLeftPosition, 50)
-    time.sleep(.2)
-    MoveBackAfterRotation(ser)
-    time.sleep(.2)
-    CommandDictToPosition(ser, standPosition, 50)
+    def Stand(self):
+        # Move all legs towards body and then stand
+        self.Sit()
+        self.__StandingPosition()
+        time.sleep(2)
 
-def MinimalRotateRightOne(ser):
-    CommandDictToPosition(ser, minimalRotateRightPosition, 50)
-    time.sleep(.2)
-    MoveBackAfterRotation(ser)
-    time.sleep(.2)
-    CommandDictToPosition(ser, standPosition, 50)
 
-# def Rotate(ser):
-    
+    def LowerHead(self, extent = 1):
+        extent *= 150
+        self.__CommandDictToPosition({self.__MT: 1550 + extent}, 200)
+
+    def WalkOneStep(self):
+        self.__CommandDictToPosition(self.__walkPositionOne, 50)
+        # time.sleep(5)
+        self.__CommandDictToPosition(self.__walkPositionTwo, 50)
+        # time.sleep(5)
+        self.__CommandDictToPosition(self.__walkPositionThree, 50)
+        # time.sleep(5)
+        self.__CommandDictToPosition(self.__walkPositionFour, 50)
+        # time.sleep(5)
+        self.__StandingPosition()
+
+    # Rotate the robot 19 degrees
+    def RotateRightOne(self):
+        self.__CommandDictToPosition(self.__rotateRightPosition, 50)
+        time.sleep(.2)
+        self.__MoveBackAfterRotation()
+        time.sleep(.2)
+        self.__CommandDictToPosition(self.__standPosition, 50)
+
+    def RotateLeftOne(self):
+        self.__CommandDictToPosition(self.__rotateLeftPosition, 50)
+        # time.sleep(.1)
+        self.__MoveBackAfterRotation()
+        # time.sleep(.1)
+        self.__CommandDictToPosition(self.__standPosition, 50)
+        
+    def MinimalRotateLeftOne(self):
+        self.__CommandDictToPosition(self.__minimalRotateLeftPosition, 50)
+        # time.sleep(.2)
+        self.__MoveBackAfterRotation()
+        # time.sleep(.2)
+        self.__CommandDictToPosition(self.__standPosition, 50)
+
+    def MinimalRotateRightOne(self):
+        self.__CommandDictToPosition(self.__minimalRotateRightPosition, 50)
+        # time.sleep(.2)
+        self.__MoveBackAfterRotation()
+        # time.sleep(.2)
+        self.__CommandDictToPosition(self.__standPosition, 50)
 
 
 def main():
-    ser = serial.Serial(scp, baudRate)
-    # CenterHeadAndTail(ser)
-    # Stand(ser)
+    motion = Motion()
+    motion.Stand()
 
-    # MoveSingleMotor(ser, LRK, 1000)
-    # RotateLeftOne(ser)
-    # RotateRightOne(ser)
-    # ReachDownAndGrab(ser)
-    # Sit(ser)
-    # Stand(ser)
-
-    # CenterHeadAndTail(ser)
-
-    # MoveSingleMotor(ser, RMK, 1800)    
-    # for i in range(0, 10):
-    #     MinimalRotateLeftOne(ser)
-    # for i in range(0, 14):
-    #     RotateRightOne(ser)
-
-    # for i in range(0, 3):
-    #     RotateLeftOne(ser)
-
-    # for i in range(0, 30):
-    #     WalkOneStep(ser)
-    # CommandThreeLeg(ser, True)
-    # CommandThreeLeg(ser, False)
-    # CommandThreeLeg(ser, True)
-    # StandingPosition(ser)
-    # for i in range(100):
-    #     CommandThreeLeg(ser, False)
-    #     CommandThreeLeg(ser, True)
-    #     CommandThreeLeg(ser, False)
-    #     CommandThreeLeg(ser, True)
-    #     CommandThreeLeg(ser, False)
-
-    #     CommandThreeLeg(ser, True)
-    #     CommandThreeLeg(ser, False)
-    #     CommandThreeLeg(ser, True)
-    #     CommandThreeLeg(ser, False)
-    # StandingPosition(ser)
-    # CommandThreeLeg(ser, True)
-    # CommandThreeLeg(ser, False)
-    # StandingPosition(ser)
-    # CommandThreeLeg(ser, True)
-    # CommandThreeLeg(ser, False)
-    # StandingPosition(ser)
-    #MoveSingleMotor(ser, LFK, 1400)
-    #MoveSingleMotor(ser, RFK , 1500)
-    #MoveSingleMotor(ser, RFH, 1550)
-    #MoveSingleMotor(ser, LFH, 1450)
-    #MoveSingleMotor(ser, LFA, 1350)
-    #MoveSingleMotor(ser, RFA, 1650)
-    #MoveSingleMotor(ser, RMK, 1350)
-    #MoveSingleMotor(ser, LMK, 1300)
-    #MoveSingleMotor(ser, RRK, 1700)
-    #MoveSingleMotor(ser, RMH, 1500)
-
-main()
+# main()
